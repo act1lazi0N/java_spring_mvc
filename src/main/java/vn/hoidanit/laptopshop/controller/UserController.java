b58bbab923e837bf7f93d8e3e06c37e7e90b6e82
Kyle Mandison <chemkhien255@gmail.com>
2024-02-27 20:29:07 +0700

    Update 27/02/2024

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,19 +2,13 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//   @RequestMapping("/")
-//   public String getHomePage() {
-//     return "Hello from Controller";
-//   }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
   private UserService userService;
@@ -23,8 +17,21 @@ public class UserController {
     this.userService = userService;
   }
 
-  @GetMapping("")
+  @RequestMapping("/")
   public String getHomePage() {
-    return this.userService.handleHello();
+    String test = this.userService.handleHello();
+    return "kyle.html";
   }
 }
+// @RestController
+// public class UserController {
+//   // DI: Dependency Injection
+//   private UserService userService;
+//   public UserController(UserService userService) {
+//     this.userService = userService;
+//   }
+//   @GetMapping("")
+//   public String getHomePage() {
+//     return this.userService.handleHello();
+//   }
+// }
