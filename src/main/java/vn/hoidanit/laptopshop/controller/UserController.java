5351eb0f67081beb857461f581b9bab485af7a03
Kyle Mandison <chemkhien255@gmail.com>
2024-03-02 13:22:45 +0700

    Update Mar 2, 2024

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,11 +2,10 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -25,16 +24,19 @@ public class UserController {
     model.addAttribute("Actilazion", "from controller with model");
     return "hello";
   }
+
+  @RequestMapping("/admin/user")
+  public String getUserPage(Model model) {
+    model.addAttribute("newUser", new User());
+    return "admin/user/create";
+  }
+
+  @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+  public String createUserPage(
+    Model mode,
+    @ModelAttribute("newUser") User actilazion
+  ) {
+    System.out.println("Run here" + actilazion);
+    return "hello";
+  }
 }
-// @RestController
-// public class UserController {
-//   // DI: Dependency Injection
-//   private UserService userService;
-//   public UserController(UserService userService) {
-//     this.userService = userService;
-//   }
-//   @GetMapping("")
-//   public String getHomePage() {
-//     return this.userService.handleHello();
-//   }
-// }
