dfbd0b857c989046bff5ad2276977ed4fa506fa1
Kyle Mandison <chemkhien255@gmail.com>
2024-04-22 12:00:33 +0700

    Update Apr 22nd, 2024

--- src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/laptopshop/config/SecurityConfiguration.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 import vn.hoidanit.laptopshop.service.CustomUserDetailsService;
 import vn.hoidanit.laptopshop.service.UserService;
 
@@ -46,6 +48,14 @@ public class SecurityConfiguration {
   }
 
   @Bean
+  public SpringSessionRememberMeServices rememberMeServices() {
+    SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+    // optionally customize
+    rememberMeServices.setAlwaysRemember(true);
+    return rememberMeServices;
+  }
+
+  @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
       .authorizeHttpRequests(authorize ->
@@ -71,13 +81,22 @@ public class SecurityConfiguration {
           .anyRequest()
           .authenticated()
       )
+      .rememberMe(r -> r.rememberMeServices(rememberMeServices()))
+      .sessionManagement(sessionManagement ->
+        sessionManagement
+          .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+          .invalidSessionUrl("/logout?expired")
+          .maximumSessions(1)
+          .maxSessionsPreventsLogin(false)
+      ) //.logout(logout>logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
       .formLogin(formLogin ->
         formLogin
           .loginPage("/login")
           .failureUrl("/login?error")
           .successHandler(CustomSuccessHandler())
           .permitAll()
-      );
+      )
+      .exceptionHandling(ex -> ex.accessDeniedPage("/access-deny"));
     return http.build();
   }
 }
---
+++ src/main/webapp/WEB-INF/view/client/auth/denied.jsp
@@ -0,0 +1,40 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+        <%@taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
+            <!DOCTYPE html>
+            <html lang="en">
+
+            <head>
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="description" content="Hỏi Dân IT - Dự án laptopshop" />
+                <meta name="author" content="Hỏi Dân IT" />
+                <title>Dashboard - Actilazion</title>
+                <link href="/css/styles.css" rel="stylesheet" />
+                <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+            </head>
+
+            <main>
+                <div class="container-fluid px-4">
+                    <div class="mt-5">
+                        <div class="row">
+                            <div class="col-12 mx-auto">
+                                <div class="alert alert-danger" role="alert">
+                                    Bạn không có quyền truy cập vào tài nguyên này!
+                                </div>
+                            </div>
+                        </div>
+                        <a href="/" class="btn btn-outline-success">Trở về</a>
+                    </div>
+
+                </div>
+            </main>
+
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+                crossorigin="anonymous"></script>
+            <script src="js/scripts.js"></script>
+            </body>
+
+            </html>
